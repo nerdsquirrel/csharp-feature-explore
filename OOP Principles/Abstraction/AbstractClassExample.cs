932706dbc6e6b2b73f66797b11dedcceb2eaff92
Mahbubur Rahman Manik <mrahmanmanik@cantaloupe.com>
2025-05-02 22:28:42 +0600

    OOP examples file added.

---
+++ OOP Principles/Abstraction/AbstractClassExample.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpFeatureExplore.OOP_Principles.Abstraction
+{
+    public abstract class NotificationService
+    {
+        public required string SenderName { get; set; }
+
+        public void Notify(string recipient, string message)
+        {
+            var formatted = FormatMessage(message);
+            Send(recipient, formatted);
+        }
+
+        protected abstract void Send(string recipient, string message);
+        protected virtual string FormatMessage(string message) => $"[INFO]: {message}";
+    }
+
+    public class EmailNotificationService : NotificationService
+    {
+        protected override void Send(string recipient, string message)
+        {
+            Console.WriteLine($"Sending Email to {recipient}: {message}");
+        }
+    }
+
+}
---
+++ OOP Principles/Abstraction/InterfaceExample.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpFeatureExplore.OOP_Principles.Abstraction
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Console: {message}");
+        }
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            // Assume writing to a file
+            Console.WriteLine($"File: {message}");
+        }
+    }
+
+}
